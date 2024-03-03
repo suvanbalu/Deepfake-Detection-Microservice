2af0008c32cb4cb1d57549fe1e1b54eeecbb5d55
suvan_balu <suvanbalu@gmail.com>
2024-03-03 21:42:41 +0530

    initial commit from local

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# Deepfake-Detection-Microservice
+# Deepfake Detection Solution
+
+## Overview
+
+This project aims to develop a robust and easily deployable solution for deepfake detection, addressing the challenges posed by the evolving nature of deepfake technology. The implemented solution includes a detection model, a Flask API, and containerization for efficient deployment.
+
+## Key Components
+
+### Detection Model
+
+The detection model is based on the EfficientB0 architecture and has been trained using 4000 fake images and 1201 real images. The training process involved 35 epochs to ensure a comprehensive learning experience.
+
+### Flask API
+
+The Flask API, implemented in the `app.py` file, serves as the interface for interacting with the deepfake detection model. It allows seamless integration into different applications, providing a user-friendly experience.
+
+You can start the API by running the following command:
+
+```bash
+pip install -r requirements.txt
+python app.py
+```
+
+You can use the `postman-collection.json` file to test the API using Postman or Thunder Client.
+
+### Docker Container
+
+Yet to be implemented.
+
+## Additional Information
+
+### Face Extraction
+
+The MTCNN (Multi-Task Cascaded Convolutional Networks) algorithm is used for face extraction as part of the preprocessing steps.
+
+### Model Training
+
+Various experiments were conducted during the model training phase to ensure optimal performance. The chosen model, trained with the specified dataset and parameters, demonstrates effectiveness in detecting deepfake content. ()
+
+## Stats
+
+Metric | Value
+--- | ---
+Total Models Trained Successfully | 26
+Total Time Taken for Model Training | 2261 minutes ~37 hrs
+Average Time Taken for Model Training | 87 minutes ~1.45 hrs
+Max Time Taken to Train a Model | 191 minutes ~3.18 hrs
+Min Number of Images Used for Training | 1601
+Max Number of Images Used for Training | 7201
+Min Number of Iterations for a Model to Train | 15
+Max Number of Iterations for a Model to Train | 50
+| Total faces extracted | 11011 |
+| Total time taken | 513 minutes ~ 8.55 hrs |
+| Total videos processed | ~3700 |
+
+## Contribution
+
+Contributions to the project are welcome. If you encounter any issues or have suggestions for improvement, feel free to submit a pull request.
---
+++ app.py
@@ -0,0 +1,140 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os
+import cv2
+import random
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.efficientnet import preprocess_input
+from mtcnn.mtcnn import MTCNN
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+model = load_model('Deepfake2d.h5')
+
+def extract_frames_rand(video_path, num_frames_to_select):
+    capture = cv2.VideoCapture(video_path)
+    total_frames = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    # Randomly select frames
+    selected_frame_indices = random.sample(range(total_frames), num_frames_to_select)
+    selected_frames = []
+
+    for frame_idx in selected_frame_indices:
+        capture.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
+        ret, frame = capture.read()
+        if ret:
+            selected_frames.append(frame)
+
+    capture.release()
+    return selected_frames
+def extract_face(frame):
+    detector = MTCNN()
+    faces = detector.detect_faces(frame)
+    if faces:
+        x, y, w, h = faces[0]['box']
+        face = frame[y:y + h, x:x + w]
+        return cv2.resize(face, (224, 224))
+    else:
+        return None
+    
+def predict_from_video(video_path,num_frame=3):
+    if not os.path.exists(video_path):
+        return "Video Not Found"
+    frames = extract_frames_rand(video_path, num_frame)
+    faces = [extract_face(frame) for frame in frames]
+    faces = [face for face in faces if face is not None]
+    if len(faces) == 0:
+        return "No Faces Detected, Try Changing the num_frame"
+    faces = np.array(faces)
+    faces = preprocess_input(faces)
+    # plt.imshow(cv2.cvtColor(faces[0], cv2.COLOR_BGR2RGB))
+    # plt.show()
+    predictions = model.predict(faces)
+    prob = predictions
+    predictions = (predictions > 0.5).astype(int)
+    return predictions,prob
+
+def predict_from_image(image_path,face_detection=True):
+    if not os.path.exists(image_path):
+        return "Image Not Found"
+    frame = cv2.imread(image_path)
+    if face_detection:
+        face = extract_face(frame)
+        if face is None:
+            return "No Face Detected"
+        face = np.expand_dims(face, axis=0)
+        face = preprocess_input(face)
+        # print(face.shape)
+        prediction = model.predict(face)
+        # prediction = (prediction > 0.5).astype(int)
+        return prediction
+    else:
+        frame = cv2.resize(frame, (224, 224))
+        frame = preprocess_input(frame)
+        frame = np.expand_dims(frame, axis=0)
+        print(frame.shape)
+        prediction = model.predict(frame)
+        print(prediction)
+        # prediction = (prediction > 0.5).astype(int)
+        return prediction
+
+@app.route('/',methods=['GET'])
+def home():
+    return "Welcome to Deepfake Detection API"
+
+@app.route('/detect_deepfake_video', methods=['POST'])
+def detect_deepfake_video():
+    if 'video' not in request.files:
+        return jsonify({"error": "No video file provided"}), 400
+
+    video_file = request.files['video']
+    video_path = 'uploaded_video.mp4'
+    video_file.save(video_path)
+
+    predictions,prob = predict_from_video(video_path)
+    os.remove(video_path)
+    predictions = np.squeeze(predictions)
+    prob = np.squeeze(prob)
+    predictions = predictions.tolist()
+    prob = prob.tolist()
+    for i in range(len(predictions)):
+        if predictions[i] == 1:
+            predictions[i] = "Fake"
+        else:
+            predictions[i] = "Real"
+            prob[i] = 1-prob[i]
+    return jsonify({"predictions": predictions,"probability":prob}), 200
+
+@app.route('/detect_deepfake_image', methods=['POST'])
+def detect_deepfake_image():
+    if 'image' not in request.files:
+        return jsonify({"error": "No image file provided"}), 400
+
+    image_file = request.files['image']
+    # print(image_file)
+    image_path = 'uploaded_image.jpg'
+    image_file.save(image_path)
+    flag = request.form.get('face_detection')
+    # print(flag)
+    if int(flag):
+        predictions = predict_from_image(image_path,face_detection=True)
+        prediction = (predictions > 0.5).astype(int)
+    else:
+        predictions = predict_from_image(image_path,face_detection=False)
+        prediction = (predictions > 0.5).astype(int)
+    # print("DSD",predictions)
+    # print("PROB",probability[0])
+    prediction = prediction[0]
+    if prediction == 1:
+        prediction = "Fake"
+        probability = predictions[0][0]
+    else:
+        prediction = "Real"
+        probability = 1-predictions[0][0]
+    os.remove(image_path)
+    return jsonify({"predictionssss": prediction,"probability":str(probability)}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
